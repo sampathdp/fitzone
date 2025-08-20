44258a82a9cd8972a0e17aaf0d455239793cb376
sampath Dananjaya <sampathdp28@gmail.com>
2025-08-20 12:08:25 +0530

    Second update

--- signin.php
+++ signin.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
@@ -100,4 +100,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </script>
 </body>
 
-</html>
+</html> -->
